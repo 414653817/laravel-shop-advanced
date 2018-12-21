55080f36cae4996dd18c878c36e0198a62a5e16b
vagrant <vagrant@homestead>
2018-12-21 05:06:58 +0000

    前台分期付款详情页

--- app/Http/Controllers/InstallmentsController.php
+++ app/Http/Controllers/InstallmentsController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 //用户分期列表
 class InstallmentsController extends Controller
 {
+    //分期
     public function index(Request $request)
     {
          $installments = Installment::query()
@@ -15,5 +16,20 @@ class InstallmentsController extends Controller
             ->paginate(10);
 
         return view('installments.index', ['installments' => $installments]);
+    }
+
+    //分期详情页
+    public function show(Installment $installment)
+    {
+        $this->authorize('own', $installment);
+        
+        // 取出当前分期付款的所有的还款计划，并按还款顺序排序
+        $items = $installment->items()->orderBy('sequence')->get();
+        return view('installments.show', [
+            'installment' => $installment,
+            'items'       => $items,
+            // 下一个未完成还款的还款计划
+            'nextItem'    => $items->where('paid_at', null)->first(),
+        ]);
     }
 }
