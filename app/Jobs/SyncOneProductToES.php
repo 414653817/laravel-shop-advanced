7a0edaf20814f75775863dcfdf7d2b7ca08feed8
vagrant <vagrant@homestead>
2018-12-26 03:23:10 +0000

    同步商品数据到 Elasticsearch

---
+++ app/Jobs/SyncOneProductToES.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+// ShouldQueue 代表异步
+//实现新增或者修改商品时，能够同步到 Elasticsearch
+class SyncOneProductToES implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function handle()
+    {
+        $data = $this->product->toESArray();
+        app('es')->index([
+            'index' => 'products',
+            'type'  => '_doc',
+            'id'    => $data['id'],
+            'body'  => $data,
+        ]);
+    }
+}
